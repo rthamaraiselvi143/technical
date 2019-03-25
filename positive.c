08237596f8cd7dbe29704b1caf67107251615db8
rthamaraiselvi143 <48053153+rthamaraiselvi143@users.noreply.github.com>
2019-03-25 14:08:29 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int A;
+    printf("enter the number A:");
+    scanf("%d",&A);
+    if(A>0)
+    printf("%d is positive number",A);
+    else
+    printf("%d is negative number",A);
+
+    return 0;
+}
